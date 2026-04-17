4780c827b806513b49c7da1f1f8679d54b7d782d
itool <itbaby@users.noreply.github.com>
2026-04-17 10:29:09 +0800

    Update hurlx to v1.0.12

--- Formula/hurlx.rb
+++ Formula/hurlx.rb
@@ -1,24 +1,24 @@
 class Hurlx < Formula
   desc "Enhanced Hurl with import/export support for HTTP testing"
   homepage "https://github.com/itbaby/hurlx"
-  version "1.0.11"
+  version "1.0.12"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/itbaby/hurlx/releases/download/v#{version}/hurlx-darwin-arm64"
-      sha256 "4b5701ec2788402e85777a9958eb31d49a847659ed68a16998f7348f7b1b888c"
+      sha256 "90a89061358298682a9459b3d3e2bc438db994a0d3f969ce6cac15a1ef5cd534"
     end
     on_intel do
       url "https://github.com/itbaby/hurlx/releases/download/v#{version}/hurlx-darwin-amd64"
-      sha256 "e25c82e9a8aaea98e6a415be8905732f778c09d5ed18407f613302cfca0486c5"
+      sha256 "ee832fafaa90a5e8dab9ee4250a473cd06adbf48da73c900c60be01b6f05fb2e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/itbaby/hurlx/releases/download/v#{version}/hurlx-linux-amd64"
-      sha256 "ad410b6774ca02e433fbc94f702ebb9b2e2ff4f3d408bbfba27113f96acd7342"
+      sha256 "ebfe066003bd3123ffd0e16afe57ef6871176ea794c8170b6b4a5b016124017c"
     end
   end
 
